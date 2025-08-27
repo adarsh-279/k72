4973433a79cdee4e46935d5c2b22bc3328010d5e
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-27 20:39:39 +0530

    Basic routing and page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Agents from './pages/Agents'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-white bg-black min-h-screen w-full'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/agents' element={<Agents />} />
+      </Routes>
+    </div>
   )
 }
 
