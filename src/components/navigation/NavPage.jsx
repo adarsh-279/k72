a748ace67d9ba3b2ae987fb81826548224926d7e
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-30 11:44:59 +0530

    Basic NavPage

---
+++ src/components/navigation/NavPage.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+
+const NavPage = () => {
+  return (
+    <div className="h-screen w-screen absolute bg-black overflow-hidden">
+      <div className="link w-full font-[l500] flex flex-col items-center justify-center mt-30 border-t-1 leading-28 relative">
+        <h1 className="text-[8vw] pt-3 text-center">WORK</h1>
+        <div className="moveLink absolute bg-[#d3fd50] text-black top-0">
+          <div className="moveX flex items-center">
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">
+              &nbsp;SEE EVERYTHING&nbsp;
+            </h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">
+              &nbsp;SEE EVERYTHING&nbsp;
+            </h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">
+              &nbsp;SEE EVERYTHING&nbsp;
+            </h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">
+              &nbsp;SEE EVERYTHING&nbsp;
+            </h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className="link w-full font-[l500] flex flex-col items-center justify-center border-t-1 leading-28 relative">
+        <h1 className="text-[8vw] pt-3 text-center">AGENCY</h1>
+        <div className="moveLink absolute bg-[#d3fd50] text-black top-0">
+          <div className="moveX flex items-center">
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;KNOW US&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/teamMembers/MEGGIE_640X290_2-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;KNOW US&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/teamMembers/PLP_640x280-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;KNOW US&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/teamMembers/MEGGIE_640X290_2-640x290.jpg"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;KNOW US&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/teamMembers/PLP_640x280-640x290.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className="link w-full font-[l500] flex flex-col items-center justify-center border-t-1 leading-28 relative">
+        <h1 className="text-[8vw] pt-3 text-center">CONTACT</h1>
+        <div className="moveLink absolute bg-[#d3fd50] text-black top-0">
+          <div className="moveX flex items-center">
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;SEND US A FAX&nbsp;</h1>
+            <FaHeart className="w-25 h-25" />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;SEND US A FAX&nbsp;</h1>
+            <FaHeart className="w-25 h-25" />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;SEND US A FAX&nbsp;</h1>
+            <FaHeart className="w-25 h-25" />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;SEND US A FAX&nbsp;</h1>
+            <FaHeart className="w-25 h-25" />
+          </div>
+        </div>
+      </div>
+      <div className="link w-full font-[l500] flex flex-col items-center justify-center border-t-1 border-b-1 leading-28 relative">
+        <h1 className="text-[8vw] pt-3 text-center">WORK</h1>
+        <div className="moveLink absolute bg-[#d3fd50] text-black top-0">
+          <div className="moveX flex items-center">
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;READ ARTICLES&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b-640x290.png"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;READ ARTICLES&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/blog/blogImg/ier.com-16107673482102220.gif"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;READ ARTICLES&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b-640x290.png"
+              alt=""
+            />
+            <h1 className="text-[8vw] whitespace-nowrap pt-3">&nbsp;READ ARTICLES&nbsp;</h1>
+            <img
+              className="h-25 w-55 rounded-full shrink-0 object-cover"
+              src="https://k72.ca/uploads/blog/blogImg/ier.com-16107673482102220.gif"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavPage;
