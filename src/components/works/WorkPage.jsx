165a37a0181dfd1a6cebfd32f5aed84207076409
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-01 11:38:59 +0530

    Work Page scroll function

--- src/components/works/WorkPage.jsx
+++ src/components/works/WorkPage.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 
-const WorkPage = ({ imgData }) => {
+const WorkPage = (props) => {
   return (
-    <div className="w-full h-120 relative overflow-hidden hover:rounded-4xl transition-all duration-200 bg-blue-400">
-      <img className="object-cover h-full w-full" src={imgData} alt="Image" />
-      <div className="opacity-0 hover:opacity-100 transition-all duration-200 absolute h-full w-full top-0 left-0 text-white flex items-center justify-center bg-black/20">
-        <h2 className="text-6xl px-5 py-2 border-1 rounded-full font-[l500] leading-9 pt-4">
-          VIEW PROJECT
-        </h2>
+    <>
+      <div className="w-full h-full relative overflow-hidden hover:rounded-4xl transition-all duration-200 gap-2">
+        <img
+          className="object-cover h-full w-full"
+          src={props.img1}
+          alt="Image"
+        />
+        <div className="opacity-0 hover:opacity-100 transition-all duration-200 absolute h-full w-full top-0 left-0 text-white flex items-center justify-center bg-black/20">
+          <h2 className="text-6xl px-5 py-2 border-1 rounded-full font-[l500] leading-9 pt-4">
+            VIEW PROJECT
+          </h2>
+        </div>
       </div>
-    </div>
+
+      <div className="w-full h-full relative overflow-hidden hover:rounded-4xl transition-all duration-200">
+        <img
+          className="object-cover h-full w-full"
+          src={props.img2}
+          alt="Image"
+        />
+        <div className="opacity-0 hover:opacity-100 transition-all duration-200 absolute h-full w-full top-0 left-0 text-white flex items-center justify-center bg-black/20">
+          <h2 className="text-6xl px-5 py-2 border-1 rounded-full font-[l500] leading-9 pt-4">
+            VIEW PROJECT
+          </h2>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,34 +1,72 @@
 import React from "react";
 import WorkPage from "../components/works/WorkPage";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const Work = () => {
   const workImg = [
-    "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/BEST/BEST_site_Thumbnail-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/OSM/thumbnailimage_OSM-1280x960.jpg",
-    "https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg",
+    {
+      img1: "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
+    },
+    {
+      img1: "https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg",
+    },
+    {
+      img1: "https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg",
+    },
+    {
+      img1: "https://k72.ca/uploads/caseStudies/BEST/BEST_site_Thumbnail-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg",
+    },
+    {
+      img1: "https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg",
+    },
+    {
+      img1: "https://k72.ca/uploads/caseStudies/OSM/thumbnailimage_OSM-1280x960.jpg",
+      img2: "https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg",
+    },
   ];
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function () {
+    gsap.from(".work", {
+      height: "200px",
+      stagger: {
+        amount: 0.95,
+      },
+      scrollTrigger: {
+        trigger: ".workParent",
+        markers: true,
+        start: "top 70%",
+        end: "top -350%",
+        scrub: true,
+      },
+    });
+  });
 
   return (
     <div className="h-screen w-full text-black p-3">
       <div className="w-full pt-75">
         <h2 className="text-[13vw] font-[l500]">WORK</h2>
       </div>
-      <div className="w-full h-600 -mt-20">
-        <div className="w-full h-120 grid grid-cols-2 gap-2 mb-5">
+        <div className="workParent w-full h-120 grid grid-cols-1 gap-2 mb-5 -mt-20">
           {workImg.map((item, index) => {
-            return <WorkPage imgData={item} key={index} />;
+            return (
+              <div
+                key={index}
+                className="work w-full h-120 flex flex-row gap-2"
+              >
+                <WorkPage img1={item.img1} img2={item.img2} />
+              </div>
+            );
           })}
         </div>
-      </div>
     </div>
   );
 };
